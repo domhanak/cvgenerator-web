84a886331905523dfa4c61a750984f64f6559a4c
domhanak <domin.hanak@gmail.com>
2014-05-24 21:46:06 +0200

    implemented servlet funcionality but i am strugling to make them apear in file

--- src/main/java/cz/muni/fi/pb138/cvgenerator/StartListener.java
+++ src/main/java/cz/muni/fi/pb138/cvgenerator/StartListener.java
@@ -40,6 +40,6 @@ public class StartListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent)
     {
-
+        System.out.print(true);
     }
 }
